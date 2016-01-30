aa11cbe987875dbd9ddc1c7473b7844b4750982d
willienn8254 <willienn@hotmail.com>
2016-01-29 21:57:39 -0500

    Crea funcionalidad listar albunes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,20 @@ class ArtistsController < ApplicationController
 		@artists=Artist.all
 
 	end
+
+
+	def show
+
+		@artist=Artist.find(params[:id])
+		@album=@artist.albums.all
+		
+
+
+	end
+
+
+	protected
+    def artist_params
+      params.require(:artist).permit(:name, :image_url)
+end
 end
